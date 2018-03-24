3e602ba685e024369303c4f219d6d8aec4fd101a
Steven Avon <savon40@gmail.com>
2018-03-24 23:25:33 +0000

    added product reference, some basic dashboard elements, still have css issues

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,41 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+    
+    section "New Products" do 
+      table_for Product.featured.limit(5) do
+        column :name do |product|
+          link_to product.name, [:admin, product]
+        end
+        column :description
+        column :category
       end
+      strong {link_to "View All Products", admin_products_path }
     end
+      
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Some Products" do
+          ul do
+            Product.limit(5)
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Info" do
+          para "Welcome to Cannabiz Network."
+        end
+      end
+    end
   end # content
 end
---
+++ app/admin/products.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Product do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+	scope :featured
+
+	index do 
+		column :id
+		column :name
+		column :description
+		column :category
+		column :featured_product
+		actions
+	end
+
+end
